49ade5da695939a6d99f4dc927837838369a251f
prashant30200501 <jangidprashant28@gmail.com>
2026-06-16 10:15:25 +0530

    add project directories

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello Card Extractor!")
+import cv2
+
+camera = cv2.VideoCapture(0)
+
+while True:
+    success, frame = camera.read()
+
+    if not success:
+        print("Could not access camera")
+        break
+
+    cv2.imshow("Card Extractor Camera", frame)
+
+    
+
+    key = cv2.waitKey(1)
+
+    if key ==ord('s'):
+        cv2.imwrite("card.jpg",frame)
+        print("image saved")
+    
+    
+
+    if key == ord('q'):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
